348d0e2283c3d0444eb3de5326676e9f43fe3a61
chaka.hu <chaka.hu@ximalaya.com>
2020-05-08 09:52:37 +0800

    环形链表

---
+++ src/main/java/com/chaka/algorithms/list/LinkedListCycle.java
@@ -0,0 +1,41 @@
+package com.chaka.algorithms.list;
+
+import com.chaka.algorithms.list.node.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 环形链表
+ *  1、hash表 （时间复杂度为O(n)）
+ *     {@code
+ *     public boolean hasCycle(ListNode head) {
+ *         Set<ListNode> set = new HashSet<>();
+ *         while (head != null){
+ *             if (set.contains(head)){
+ *                 return true;
+ *             }
+ *             set.add(head);
+ *             head = head.next;
+ *         }
+ *         return false;
+ *     }}
+ *  2、快慢指针
+ */
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast){return true;}
+        }
+        return false;
+    }
+
+}
