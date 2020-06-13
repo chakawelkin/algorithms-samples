2399bd57192441fdb4d8b1a4a097e1eb2bd792f5
chaka.hu <chaka.hu@ximalaya.com>
2020-06-13 10:23:58 +0800

    组合

--- src/main/java/com/chaka/algorithms/recursion/Combinations.java
+++ src/main/java/com/chaka/algorithms/recursion/Combinations.java
@@ -1,6 +1,8 @@
 package com.chaka.algorithms.recursion;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * <a href="https://leetcode-cn.com/problems/combinations/">组合</a>
@@ -8,7 +10,24 @@ import java.util.List;
  */
 public class Combinations {
 
+    private List<List<Integer>> list = new ArrayList<>();
+
     public List<List<Integer>> combine(int n, int k) {
+        reCombine(n,k,1,new Stack<>());
+        return list;
+    }
+
+    public void reCombine(int n, int k, int begin, Stack<Integer> stack){
+        if (k == stack.size()){
+            list.add(new ArrayList<>(stack));
+            return;
+        }
+        //这个地方需要剪枝
+        for (int i = begin; i <= n && (k - stack.size()) <= n + 1 - i; i++) {
+            stack.push(i);
+            reCombine(n,k,i + 1,stack);
+            stack.pop();
+        }
 
     }
 
