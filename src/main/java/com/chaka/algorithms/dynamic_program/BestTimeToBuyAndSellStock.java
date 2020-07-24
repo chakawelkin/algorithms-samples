53101971672bd11b0cd6fced2676fb414645fbfe
chaka.hu <chaka.hu@ximalaya.com>
2020-07-24 08:20:25 +0800

    股票买卖的最佳时机

---
+++ src/main/java/com/chaka/algorithms/dynamic_program/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,33 @@
+package com.chaka.algorithms.dynamic_program;
+
+/**
+ * <a href="https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock">最佳买卖股票时机</a>
+ * 一、今日手上无股票
+ *  1、昨日就没有持有股票，今天选择继续不持有，所以无股票
+ *  2、要么是昨天持有股票，今天卖出了
+ *  dp[i][k][0] = max(dp[i-1][k][0],dp[i-1][k][1] + price[i])
+ * 二、手上有股票
+ *  1、昨日持有股票，今天不卖，继续持有
+ *  2、昨日无股票，今天买入
+ *  dp[i][k][1] = max(dp[i-1][k][1],dp[i-1][k-1][0] - price[i])
+ *
+ *  买入时，最大交易次数减一
+ */
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0){
+            return 0;
+        }
+        int n = prices.length;
+        //int[][] dp = new int[n][2];
+        int dp_i_0 = 0;
+        int dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            dp_i_0 = Math.max(dp_i_0,dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, - prices[i]);
+        }
+        return dp_i_0;
+    }
+
+}
---
+++ src/main/java/com/chaka/algorithms/dynamic_program/HouseRobberThree.java
@@ -0,0 +1,17 @@
+package com.chaka.algorithms.dynamic_program;
+
+import com.chaka.algorithms.tree.node.TreeNode;
+
+/**
+ * <a href="https://leetcode-cn.com/problems/house-robber-iii/">打家劫舍三</a>
+ * 1、第1间 -> 倒数第2间
+ * 2、第2间 -> 最后1间
+ */
+public class HouseRobberThree {
+
+    public int rob(TreeNode root) {
+        //使用dp table记录偷过的房间
+        return 0;
+    }
+
+}
