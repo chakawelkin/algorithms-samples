a60a1661a013e637f4fdc708cec423f6aad2c1ac
chaka.hu <chaka.hu@ximalaya.com>
2020-07-29 17:53:42 +0800

    最佳买卖股票时机含冷冻期

--- src/main/java/com/chaka/algorithms/dynamic_program/BestTimeToBuyAndSellStockTwo.java
+++ src/main/java/com/chaka/algorithms/dynamic_program/BestTimeToBuyAndSellStockTwo.java
@@ -10,17 +10,14 @@ public class BestTimeToBuyAndSellStockTwo {
 
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
+//        int[][] dp = new int[n][2];
+        int dp_i_0 = 0;
+        int dp_i_1 = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
-            if (i == 0){
-                dp[0][0] = 0;
-                dp[0][1] = -prices[i];
-                continue;
-            }
-            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0] - prices[i]);
+            dp_i_0 = Math.max(dp_i_0,dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1,dp_i_0 - prices[i]);
         }
-        return dp[n-1][0];
+        return dp_i_0;
     }
 
 }
