e110ced5ebe514618a88e73d7b3c752aed0706e4
chaka.hu <chaka.hu@ximalaya.com>
2020-07-31 14:03:58 +0800

    实现Trie树

--- src/main/java/com/chaka/algorithms/dictionary_tree/Trie.java
+++ src/main/java/com/chaka/algorithms/dictionary_tree/Trie.java
@@ -2,28 +2,61 @@ package com.chaka.algorithms.dictionary_tree;
 
 /**
  * <a href="https://leetcode-cn.com/problems/implement-trie-prefix-tree/">实现 Trie (前缀树)</a>
- *
+ *  1、实现trie树
  */
 public class Trie {
 
-    /** Initialize your data structure here. *//*
+    private TrieNode root;
+
     public Trie() {
-
+        root = new TrieNode();
     }
 
-    *//** Inserts a word into the trie. *//*
     public void insert(String word) {
-
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+           if (node.next[c - 'a'] == null){
+               node.next[c - 'a'] = new TrieNode();
+           }
+           node = node.next[c - 'a'];
+        }
+        node.isEnd = true;
     }
 
-    *//** Returns if the word is in the trie. *//*
     public boolean search(String word) {
-
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if ((node = node.next[c - 'a']) == null){
+                return false;
+            }
+        }
+        return node.isEnd;
     }
 
-    *//** Returns if there is any word in the trie that starts with the given prefix. *//*
     public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if ((node = node.next[c - 'a']) == null){
+                return false;
+            }
+        }
+        return true;
+    }
 
-    }*/
+    /**
+     *
+     */
+    static class TrieNode{
+
+        public boolean isEnd;
+
+        public TrieNode[] next;
+
+        public TrieNode() {
+            this.isEnd = false;
+            this.next = new TrieNode[26];
+        }
+
+    }
 
 }
