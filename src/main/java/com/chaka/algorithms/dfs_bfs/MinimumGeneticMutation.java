9c40cd3bac4b0c4c4b5420d04da163095207fcb5
chaka.hu <chaka.hu@ximalaya.com>
2020-08-16 09:39:14 +0800

    DFS

--- src/main/java/com/chaka/algorithms/dfs_bfs/MinimumGeneticMutation.java
+++ src/main/java/com/chaka/algorithms/dfs_bfs/MinimumGeneticMutation.java
@@ -1,12 +1,54 @@
 package com.chaka.algorithms.dfs_bfs;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * <a href="https://leetcode-cn.com/problems/minimum-genetic-mutation/">最小基因变化</a>
  */
 public class MinimumGeneticMutation {
 
+    private int minStep = Integer.MAX_VALUE;
+
     public int minMutation(String start, String end, String[] bank) {
-        return -1;
+        if (start == null || end == null || bank == null){
+            return -1;
+        }
+        int step = 0;
+        Set<String> visited = new HashSet<>();
+        dfs(step,start,end,bank,visited);
+        return minStep == Integer.MAX_VALUE ? -1 : minStep;
+    }
+
+    private void dfs(int step, String start, String end, String[] bank, Set<String> visited) {
+        if (start.equals(end)){
+            minStep = Math.min(minStep,step);
+            return;
+        }
+        for (String gene : bank) {
+            int diff = 0;
+            for (int i = 0; i < gene.length(); i++) {
+                if (start.charAt(i) != gene.charAt(i)){
+                    diff++;
+                }
+                if (diff > 1){
+                    break;
+                }
+            }
+
+            if (diff == 1 && !visited.contains(gene)){
+                visited.add(gene);
+                dfs(step + 1, gene, end, bank, visited);
+                visited.remove(gene);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        MinimumGeneticMutation sol = new MinimumGeneticMutation();
+        int res = sol.minMutation("AACCGGTT", "AAACGGTA", new String[]{"AACCGGTA", "AACCGCTA", "AAACGGTA"});
+        System.out.println(res);
     }
 
 }
--- src/main/java/com/chaka/algorithms/dfs_bfs/NumberOfIslands.java
+++ src/main/java/com/chaka/algorithms/dfs_bfs/NumberOfIslands.java
@@ -7,16 +7,49 @@ package com.chaka.algorithms.dfs_bfs;
  */
 public class NumberOfIslands {
 
-    private int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+    private int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private int m, n;
+    private int count;
 
     public int numIslands(char[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        for (int i = 0; i < m; i++){
+        if(grid == null || grid.length == 0){
+            return count;
+        }
+        m = grid.length;
+        n = grid[0].length;
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-
+                if (grid[i][j] == '0') {
+                    continue;
+                }
+                dfs(grid, i, j);
+                count++;
             }
         }
-        return 0;
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if(i < 0 || j < 0 || i >= m ||j >= n){
+            return;
+        }
+        if (grid[i][j] == '0'){
+            return;
+        }
+        grid[i][j] = '0';
+        for (int[] dir : dirs) {
+            dfs(grid,i + dir[0],j + dir[1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = new char[][]{
+                {'1','1','0'},
+                {'1','1','0'},
+                {'0','0','1'},
+        };
+        NumberOfIslands sol = new NumberOfIslands();
+        System.out.println(sol.numIslands(grid));
     }
 
 }
