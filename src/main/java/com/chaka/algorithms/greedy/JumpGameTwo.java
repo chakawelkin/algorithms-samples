3d0ca4c167a509aad39f00a75ec0d4bcf9f52ead
chaka.hu <chaka.hu@ximalaya.com>
2020-08-16 16:08:16 +0800

    跳跃游戏 II --贪心

--- src/main/java/com/chaka/algorithms/greedy/JumpGameTwo.java
+++ src/main/java/com/chaka/algorithms/greedy/JumpGameTwo.java
@@ -4,4 +4,19 @@ package com.chaka.algorithms.greedy;
  * <a href="https://leetcode-cn.com/problems/jump-game-ii/">跳跃游戏 II</a>
  */
 public class JumpGameTwo {
+
+    public int jump(int[] nums) {
+        int end = 0, rightMost=0, steps=0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i <= rightMost){
+                rightMost = Math.max(rightMost,i + nums[i]);
+                if (i == end){//每次走最远的位置
+                    end = rightMost;
+                    steps++;
+                }
+            }
+        }
+        return steps;
+    }
+
 }
