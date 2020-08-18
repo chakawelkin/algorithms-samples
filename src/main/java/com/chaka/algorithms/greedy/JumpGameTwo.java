5015085af2139bb1e9402f5ad04eb84cb5f04b9e
chaka.hu <chaka.hu@ximalaya.com>
2020-08-18 08:22:44 +0800

    柠檬水找零 --贪心

--- src/main/java/com/chaka/algorithms/greedy/JumpGameTwo.java
+++ src/main/java/com/chaka/algorithms/greedy/JumpGameTwo.java
@@ -2,11 +2,14 @@ package com.chaka.algorithms.greedy;
 
 /**
  * <a href="https://leetcode-cn.com/problems/jump-game-ii/">跳跃游戏 II</a>
+ *
+ * 该提 -- 假设是总是可以跳到最后的位置
  */
 public class JumpGameTwo {
 
     public int jump(int[] nums) {
         int end = 0, rightMost=0, steps=0;
+        //因为第一步的时候就已经记录了一次
         for (int i = 0; i < nums.length - 1; i++) {
             if (i <= rightMost){
                 rightMost = Math.max(rightMost,i + nums[i]);
--- src/main/java/com/chaka/algorithms/greedy/LemonadeChange.java
+++ src/main/java/com/chaka/algorithms/greedy/LemonadeChange.java
@@ -5,4 +5,30 @@ package com.chaka.algorithms.greedy;
  */
 public class LemonadeChange {
 
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5){
+                five++;
+                continue;
+            }
+            if (bills[i] == 10){
+                ten++;
+                five--;
+            }
+            if (bills[i] == 20){
+                if (ten > 0){
+                    ten--;
+                    five--;
+                }else {
+                    five = five - 3;
+                }
+            }
+            if (five < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
