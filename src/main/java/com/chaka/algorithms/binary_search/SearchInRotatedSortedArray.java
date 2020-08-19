15f69952e6cc70a8f56a7d54b01464c0da0b1432
chaka.hu <chaka.hu@ximalaya.com>
2020-08-19 19:02:51 +0800

    搜索旋转排序数组

--- src/main/java/com/chaka/algorithms/binary_search/SearchInRotatedSortedArray.java
+++ src/main/java/com/chaka/algorithms/binary_search/SearchInRotatedSortedArray.java
@@ -2,6 +2,36 @@ package com.chaka.algorithms.binary_search;
 
 /**
  * <a href="https://leetcode-cn.com/problems/search-in-rotated-sorted-array/">搜索旋转排序数组</a>
+ * 1、先找到旋转数组最小值的索引，然后再进行二分查找
  */
 public class SearchInRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+        int left  = 0;
+        int right = nums.length - 1;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+
+            if (target >= nums[0]){
+                if (nums[mid] < nums[0]){
+                    nums[mid] = Integer.MAX_VALUE;
+                }
+            }else {
+                if (nums[mid] >= nums[0]){
+                    nums[mid] = Integer.MIN_VALUE;
+                }
+            }
+
+            if (nums[mid] < target){
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
 }
