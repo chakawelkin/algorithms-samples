77ec2d5f065fbab8b72f59806af93ba83a8f6ffb
chaka.hu <chaka.hu@ximalaya.com>
2020-08-28 08:08:30 +0800

    位1的个数 & 颠倒二进制位

--- src/main/java/com/chaka/algorithms/bit_operation/NumberOf1Bits.java
+++ src/main/java/com/chaka/algorithms/bit_operation/NumberOf1Bits.java
@@ -5,4 +5,22 @@ package com.chaka.algorithms.bit_operation;
  */
 public class NumberOf1Bits {
 
+    public int hammingWeight(int n) {
+        int bit = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i++) {
+            //与运算
+            if ((mask & n) != 0){
+                bit++;
+            }
+            mask = mask << 1;
+        }
+        return bit;
+    }
+
+    public static void main(String[] args) {
+        NumberOf1Bits sol = new NumberOf1Bits();
+        System.out.println(sol.hammingWeight(11));
+    }
+
 }
--- src/main/java/com/chaka/algorithms/bit_operation/ReverseBits.java
+++ src/main/java/com/chaka/algorithms/bit_operation/ReverseBits.java
@@ -4,4 +4,25 @@ package com.chaka.algorithms.bit_operation;
  * <a href="https://leetcode-cn.com/problems/reverse-bits/">颠倒二进制位</a>
  */
 public class ReverseBits {
+
+    public int reverseBits(int n) {
+        if(n == 0){
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result = result << 1;
+            if ((n & 1) == 1){
+                result++;
+            }
+            n = n >> 1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ReverseBits reverse = new ReverseBits();
+        System.out.println(reverse.reverseBits(5));
+    }
+
 }
