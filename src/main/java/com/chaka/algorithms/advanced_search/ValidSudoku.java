1733df1d321c44a70d5f1bc87b4c642643092d6d
chaka.hu <chaka.hu@ximalaya.com>
2020-10-15 07:54:38 +0800

    有效的数独 rename

--- src/main/java/com/chaka/algorithms/advanced_search/ValidSudoku.java
+++ src/main/java/com/chaka/algorithms/advanced_search/ValidSudoku.java
@@ -3,7 +3,7 @@ package com.chaka.algorithms.advanced_search;
 /**
  * <a href="https://leetcode-cn.com/problems/valid-sudoku/description/">有效的数独</a>
  */
-public class VlidSudoku {
+public class ValidSudoku {
 
     public boolean isValidSudoku(char[][] board) {
         int[][] row = new int[9][10];
